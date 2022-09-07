4a0a44d1c5d0c4a8e80d1ea789bf9f3b78c3496f
Jaime <jaimehiguerasmedina98@gmail.com>
2022-09-07 11:05:26 +0200

    Dejo solo el archivo git config-data

--- usuario-service/src/main/java/usuarioservice/usuarioservice/repositorios/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package usuarioservice.usuarioservice.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import usuarioservice.usuarioservice.modelos.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Integer> { }
